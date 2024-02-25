4bd376d5c27409662966990c82133e93feb38d38
Alex <alexnowrang@gmail.com>
2024-02-25 09:46:03 -0600

    Delete unnecessary code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,2 @@
 import './style.css';
-import { loadHeader, loadContentContainer } from './home';
-
-const header = loadHeader('Alex');
-
-const contentContainer = loadContentContainer();
-
-const bodyArr = [header, contentContainer];
-
-bodyArr.forEach(e => document.body.appendChild(e));
-
 
