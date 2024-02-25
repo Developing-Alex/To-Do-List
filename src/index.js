790722c1f894306653b620d456e6cc46c648ae26
Alex <alexnowrang@gmail.com>
2024-02-25 17:05:00 -0600

    Update styles, add click events to navigate app

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
 import './style.css';
+import ShowAllTasks from './home';
+import ShowDailyTasks from './daily';
+import ShowWeeklyTasks from './weekly';
+
+function LoadHomePageContent(){
+  const allBtn = document.getElementById('all-btn');
+  
+  allBtn.onclick = ShowAllTasks;
+}
+
+LoadHomePageContent();
+
+function LoadDailyTasks(){
+  const dailyBtn = document.getElementById('day-btn');
+
+  dailyBtn.onclick = ShowDailyTasks;
+}
+
+LoadDailyTasks();
+
+function LoadWeeklyTasks(){
+  const weekBtn = document.getElementById('week-btn');
+
+  weekBtn.onclick = ShowWeeklyTasks;
+}
+
+LoadWeeklyTasks();
 
