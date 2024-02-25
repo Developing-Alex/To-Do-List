bf29c9a1235b529ce04ce69bef0b7709c591d59e
Alex <alexnowrang@gmail.com>
2024-02-25 09:43:05 -0600

    Remove htmlwebpackplugin, hard code html

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,11 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Pencil It In',
-    }),
-  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
   },
   module: {
     rules: [
