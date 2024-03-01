56553dd3dce57d7389f75c8caf77083b358c99ac
Alex <alexnowrang@gmail.com>
2024-02-29 20:41:15 -0600

    Add function to display tasks on UI

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './style.css';
+import createTask from './function';
+import { taskHandler } from './function';
 
 const allBtn = document.getElementById('all-btn');
 const dailyBtn = document.getElementById('day-btn');
@@ -52,18 +54,23 @@ function ShowWeeklyTasks() {
 weekBtn.onclick = ShowWeeklyTasks;
 
 const addTaskContainer = document.querySelectorAll('.add-task-container');
+const addTaskParagraph = document.querySelectorAll('.add-task-paragraph');
 
 function createFormContainer(taskContainer) {
-
-  const formBox = document.createElement('div');
-  formBox.id = 'form-box';
+  const formBox = document.createElement('form');
+  formBox.name = `form-box`;
+  formBox.id = formBox.name;  
+  formBox.method = 'post';
+  formBox.action = '';
 
   const formTitle = document.createElement('textarea');
-  formTitle.id = 'form-title';
+  formTitle.id= 'form-title';
   formTitle.name = formTitle.id;
   formTitle.cols = '30';
   formTitle.row = '1';
   formTitle.placeholder = 'Title';
+  formTitle.required = true;
+  formTitle.value = '';
 
   const formDescription = document.createElement('textarea');
   formDescription.id = 'form-description';
@@ -71,6 +78,7 @@ function createFormContainer(taskContainer) {
   formDescription.cols = '30';
   formDescription.row = '3';
   formDescription.placeholder = 'Description';
+  formDescription.required = true;
 
   const priorityHeader = document.createElement('h4');
   priorityHeader.id = 'priority-header';
@@ -87,11 +95,11 @@ function createFormContainer(taskContainer) {
   highInput.name = 'priority';
   highInput.id = 'high';
   highInput.className = 'priority-radio';
-  highInput.value = highInput.id;
 
   const highLabel = document.createElement('label');
   highLabel.textContent = 'High';
   highLabel.setAttribute('for', 'high');
+  highInput.value = highLabel.textContent;
 
   const highPriorityArr = [highInput, highLabel];
   highPriorityArr.forEach(e => highPriority.appendChild(e));
@@ -104,11 +112,11 @@ function createFormContainer(taskContainer) {
   mediumInput.name = 'priority';
   mediumInput.id = 'medium';
   mediumInput.className = 'priority-radio';
-  mediumInput.value = mediumInput.id;
 
   const mediumLabel = document.createElement('label');
   mediumLabel.textContent = 'Medium';
   mediumLabel.setAttribute('for', 'medium');
+  mediumInput.value = mediumLabel.textContent;
 
   const mediumPriorityArr = [mediumInput, mediumLabel];
   mediumPriorityArr.forEach(e => mediumPriority.appendChild(e));
@@ -121,11 +129,11 @@ function createFormContainer(taskContainer) {
   lowInput.name = 'priority';
   lowInput.id = 'low';
   lowInput.className = 'priority-radio';
-  lowInput.value = lowInput.id;
 
   const lowLabel = document.createElement('label');
   lowLabel.textContent = 'Low'
   lowLabel.setAttribute('for', 'low');
+  lowInput.value = lowLabel.textContent;
 
   const lowPriorityArr = [lowInput, lowLabel];
   lowPriorityArr.forEach(e => lowPriority.appendChild(e));
@@ -135,25 +143,92 @@ function createFormContainer(taskContainer) {
 
   const dueDateBtn = document.createElement("button");
   dueDateBtn.id = 'due-date-btn';
-  dueDateBtn.textContent = 'Due Date';
+  dueDateBtn.className = 'form-btn';
+  dueDateBtn.textContent = 'Add Due Date';
 
-  const formBoxArr = [formTitle, formDescription, priorityHeader, priorityContainer, dueDateBtn];
+  const dateInput = document.createElement('input');
+  dateInput.id = 'date-input';
+  dateInput.name = dateInput.id;
+  dateInput.type = 'date';
+
+  dueDateBtn.addEventListener('click', () => {
+    formBox.replaceChild(dateInput, dueDateBtn)
+  })
+
+  const submitTaskBtn = document.createElement('input');
+  submitTaskBtn.id = 'submit';
+  submitTaskBtn.className = 'form-btn';
+  submitTaskBtn.type = 'submit';
+
+  const formBoxArr = [formTitle, formDescription, priorityHeader, priorityContainer, dueDateBtn, submitTaskBtn];
   formBoxArr.forEach(e => formBox.appendChild(e));
 
-  taskContainer.prepend(formBox);
+  taskContainer.appendChild(formBox);
+
+  submitTaskBtn.addEventListener('click', function(event){
+    event.preventDefault();
+
+    const taskTitle = formTitle.value;
+    const taskDescription = formDescription.value;
+    let taskPriority = document.querySelector('input[name="priority"]:checked').value
+    const taskDueDate = dateInput.value;
+
+    submitTaskBtn.parentElement.style.display = 'none';
+    addTaskParagraph.forEach(btn => btn.style.display = 'flex')
+    taskContainer.prepend(displayTasks(createTask(taskTitle, taskDescription,taskPriority,taskDueDate)));
+
+    console.log(taskPriority)
+
+    formTitle.value = '';
+    formDescription.value = '';
+    taskPriority.value = '';
+    dateInput.value = '';
+  })
 
   return taskContainer;
 }
 
-const addTaskParagraph = document.querySelectorAll('.add-task-paragraph');
-
 addTaskParagraph.forEach(btn => {
   btn.addEventListener('click', () => {
     if (btn.parentElement.id.charAt(btn.parentElement.id.length - 1) === btn.id.charAt(btn.id.length - 1))
-      createFormContainer(btn.parentElement)
+      createFormContainer(btn.parentElement);
+      btn.style.display = 'none';
   })
 })
 
 
+function displayTasks(obj){
 
-//export { LoadDailyTasks, LoadAllTasks, LoadWeeklyTasks }
+  const taskCard = document.createElement('div');
+  taskCard.id = `task-card`
+  taskCard.className = 'task-cards';
+
+  const taskTitle = document.createElement('p');
+  taskTitle.id = `task-title`;
+  taskTitle.className = 'task-titles';
+  
+  const taskDescription = document.createElement('p');
+  taskDescription.id = `task-description`;
+  taskDescription.className = 'task-descriptions';
+
+  const taskPriority = document.createElement('p');
+  taskPriority.id = `task-priority`;
+  taskPriority.className = 'task-priorities';
+
+  const taskDueDate = document.createElement('p');
+  taskDueDate.id = `task-due-date`;
+  taskDueDate.className = 'task-due-dates';
+
+  const taskCardArr = [taskTitle, taskDescription, taskPriority, taskDueDate];
+  taskCardArr.forEach(taskCardEl => {
+    taskCard.appendChild(taskCardEl);
+  });
+
+  taskTitle.textContent = `Task: ${obj.title}`;
+  taskDescription.textContent = `Details: ${obj.description}`;
+  taskPriority.textContent = `Priority: ${obj.priority}`;
+  taskDueDate.textContent = `Due date: ${obj.dueDate}`;
+
+ return taskCard
+};
+
